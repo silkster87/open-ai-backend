d2653691a4da777b01dc7e419c746744ee701257
Silky Ng <silky.ng@gmail.com>
2023-05-19 16:40:27 +0100

    setting up ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
 
 @Module({
-  imports: [OpenaiModule],
+  imports: [ConfigModule.forRoot(), OpenaiModule],
   controllers: [AppController],
   providers: [AppService],
 })
