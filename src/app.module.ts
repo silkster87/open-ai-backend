a7042332bf88935cdd57c4bc2c2ea30f24ccc7fd
Silky Ng <silky.ng@gmail.com>
2023-05-20 10:07:40 +0100

    configuration of MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), OpenaiModule],
+  imports: [
+    ConfigModule.forRoot(),
+    OpenaiModule,
+    MongooseModule.forRoot('mongodb://localhost/openai'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
