467b7a8e5beb3fca68170ca00901fac48aa2069c
Silky Ng <silky.ng@gmail.com>
2023-05-20 11:10:17 +0100

    setting up providers for testing

--- src/openai/openai.controller.spec.ts
+++ src/openai/openai.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
 import { OpenaiController } from './openai.controller';
 import { OpenaiService } from './openai.service';
+import { Model } from 'mongoose';
+import { Summary } from './schemas/summary.schema';
 
 describe('OpenaiController', () => {
   let controller: OpenaiController;
@@ -8,7 +11,10 @@ describe('OpenaiController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OpenaiController],
-      providers: [OpenaiService],
+      providers: [
+        { provide: getModelToken(Summary.name), useValue: Model },
+        OpenaiService,
+      ],
     }).compile();
 
     controller = module.get<OpenaiController>(OpenaiController);
