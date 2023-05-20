bc67672aced63d3ad034f87466100f9459c75fcd
Silky Ng <silky.ng@gmail.com>
2023-05-20 10:09:09 +0100

    mock data from json response for testing

---
+++ src/openai/mockdata/summary-response.ts
@@ -0,0 +1,19 @@
+export const summary = {
+  id: 'cmpl-7IBfhsTHuLSjtxKB5qdnuTTLg7wsm',
+  object: 'text_completion',
+  created: 1684569125,
+  model: 'text-davinci-003',
+  choices: [
+    {
+      text: "\n\nLots of people in the UK are having health problems that make it difficult to work. In 2016, 15.4% of people aged 16-64 had these kinds of issues, and this number grew to 18.1% in 2022 - that's about 7.5 million people.",
+      index: 0,
+      logprobs: null,
+      finish_reason: 'stop',
+    },
+  ],
+  usage: {
+    prompt_tokens: 83,
+    completion_tokens: 59,
+    total_tokens: 142,
+  },
+};
