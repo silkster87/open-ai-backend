2e628aeea43728376e98aecce9cd4c8e6bb52b94
Silky Ng <silky.ng@gmail.com>
2023-05-23 09:56:29 +0100

    using mock for time being and made remove()

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -5,6 +5,7 @@ import { CreateOpenaiDto } from './dto/create-openai.dto';
 import { Summary } from './schemas/summary.schema';
 import { Configuration, OpenAIApi } from 'openai';
 import { summary } from './mockdata/summary-response';
+import mongodb from 'mongodb';
 
 @Injectable()
 export class OpenaiService {
@@ -12,40 +13,43 @@ export class OpenaiService {
     @InjectModel(Summary.name) private summaryModel: Model<Summary>,
   ) {}
   async createSummary(createOpenaiDto: CreateOpenaiDto): Promise<Summary> {
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
+    return new Promise((resolve) => {
+      const { text } = summary.choices[0];
+      const { created } = summary;
+      resolve({ text, created });
     });
+    // const configuration = new Configuration({
+    //   apiKey: process.env.OPENAI_API_KEY,
+    // });
 
-    const openai = new OpenAIApi(configuration);
+    // const openai = new OpenAIApi(configuration);
 
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `Summarize this for a second-grade student:\n\n${createOpenaiDto.text}`,
-      temperature: 0.7,
-      max_tokens: 64,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
+    // const response = await openai.createCompletion({
+    //   model: 'text-davinci-003',
+    //   prompt: `Summarize this for a second-grade student:\n\n${createOpenaiDto.text}`,
+    //   temperature: 0.7,
+    //   max_tokens: 64,
+    //   top_p: 1.0,
+    //   frequency_penalty: 0.0,
+    //   presence_penalty: 0.0,
+    // });
 
-    const { text } = response.data.choices[0];
-    const { created } = response.data;
-    //const { text } = summary.choices[0];
-    //const { created } = summary;
-    const createSummaryDto = { text, created };
+    // const { text } = response.data.choices[0];
+    // const { created } = response.data;
+    // //const { text } = summary.choices[0];
+    // //const { created } = summary;
+    // const createSummaryDto = { text, created };
 
-    const createdSummary = new this.summaryModel(createSummaryDto);
+    // const createdSummary = new this.summaryModel(createSummaryDto);
 
-    return createdSummary.save();
-
-    //return summary;
+    // return createdSummary.save();
   }
 
   async findAll(): Promise<Summary[]> {
     return this.summaryModel.find().exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} openai`;
+  async remove(id: string) {
+    return this.summaryModel.deleteOne({ _id: new mongodb.ObjectId(id)});
   }
 }
